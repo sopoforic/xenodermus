bdbc7b88892d191783b34a82aee2996216c93942
Tracy Poff <tracy.poff@gmail.com>
2016-11-19 10:00:31 -0500

    Bump version to 0.3.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='xenodermus',
-      version='0.2',
+      version='0.3',
       description='Simple file storage library',
       author='Tracy Poff',
       author_email='tracy.poff@gmail.com',
